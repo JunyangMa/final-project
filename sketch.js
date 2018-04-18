2a2179e75b0d93dc16edec525797153a15baf9e6
JunyangMa <38508103+JunyangMa@users.noreply.github.com>
2018-04-18 16:26:57 -0400

    1

--- sketch.js
+++ sketch.js
@@ -1,11 +1,13 @@
 var world;
 
 var markerZB;
+var markerHiro;
 
 function setup() {
   world = new World('ARScene');
 
   markerZB = world.getMarker('zb');
+  markerHiro = world.getMarker('hiro');
 
   // the marker is 1 meter x 1 meter, with the origin at the center
   // the x-axis runs left to right, z-xaxis runs top to bottom, y-axis runs up/down (through the marker)
@@ -14,7 +16,7 @@ function setup() {
   //   x: 0,
   //   y: 0,
   //   z: 0,
-  //   radius: 0.5,
+  //   radius: 0.5,    8.7
   //   height: 10,
   //   red: random(255),
   //   green: random(255),
@@ -27,12 +29,9 @@ function setup() {
     x: 0,
     y: 0,
     z: -5,
-    width: 3,     //150,230
+    width: 3,
     height: 4.6,
     asset:'planeAsset',
-    // red: random(255),
-    // green: random(255),
-    // blue: random(255),
     rotationX: -90,
   });
   markerZB.addChild(plane);
