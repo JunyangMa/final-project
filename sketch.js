4690958ef35f59e672edca60a02ecacf01192807
JunyangMa <38508103+JunyangMa@users.noreply.github.com>
2018-04-18 20:48:05 -0400

    3

--- sketch.js
+++ sketch.js
@@ -2,39 +2,60 @@ var world;
 
 var markerZB;
 var markerHiro;
+var markerThird;
 
 function setup() {
   world = new World('ARScene');
 
   markerZB = world.getMarker('zb');
   markerHiro = world.getMarker('hiro');
+  markerThird = world.getMarker('third');
+
 
   // the marker is 1 meter x 1 meter, with the origin at the center
   // the x-axis runs left to right, z-xaxis runs top to bottom, y-axis runs up/down (through the marker)
+  var cylinder = new Cylinder({
+    x: 0,
+    y: -1,
+    z: -17,
+    radius: 0.435,    //8.7
+    height: 35.6,     //712
+    asset:'cylinderAsset',
+    rotationX: 90
+  });
+  markerZB.addChild(cylinder);
 
-  // var cylinder = new Cylinder({
+  var cylinder2 = new Cylinder({
+    x: 0,
+    y: -1,
+    z: -17,
+    radius: 0.435,    //8.7
+    height: 35.6,     //712
+    asset:'cylinderAsset',
+    rotationX: 90
+  });
+  markerHiro.addChild(cylinder2);
+
+  var cylinder3 = new Cylinder({
+    x: 0,
+    y: 17,
+    z: 0,
+    radius: 0.435,    //8.7
+    height: 35.6,     //712
+    asset:'cylinderAsset',
+  });
+  markerThird.addChild(cylinder3);
+
+  // var plane = new Plane({
   //   x: 0,
   //   y: 0,
-  //   z: 0,
-  //   radius: 0.5,    8.7
-  //   height: 10,
-  //   red: random(255),
-  //   green: random(255),
-  //   blue: random(255),
-  //   rotationX: 90,
+  //   z: -5,
+  //   width: 3,
+  //   height: 4.6,
+  //   asset:'planeAsset',
+  //   rotationX: -90
   // });
-  // markerZB.addChild(cylinder);
-
-  var plane = new Plane({
-    x: 0,
-    y: 0,
-    z: -5,
-    width: 3,
-    height: 4.6,
-    asset:'planeAsset',
-    rotationX: -90,
-  });
-  markerZB.addChild(plane);
+  // markerZB.addChild(plane);
 
 }
 
